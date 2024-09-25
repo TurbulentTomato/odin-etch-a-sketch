a4548574a9a97af628735b3c7213b214fa6142e3
TurbulentTomato <abhinavgangwar87@gmail.com>
2024-09-25 09:58:27 +0530

    Change variable names
    Changed container variable because of changes in html structure

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-const container = document.querySelector(".container");
+const gridContainer = document.querySelector(".grid-container");
 let divNumber = 16; //no. of divs in grid
 for (divNumber = divNumber ** 2; divNumber > 0; divNumber--) {
   const gridDiv = document.createElement("div");
   gridDiv.setAttribute("class", "grid-div");
-  container.appendChild(gridDiv);
+  gridContainer.appendChild(gridDiv);
 
 }
-container.addEventListener("mouseover", (event) => {
+gridContainer.addEventListener("mouseover", (event) => {
   const red = Math.floor(Math.random() * 266);
   const green = Math.floor(Math.random() * 266);
   const blue = Math.floor(Math.random() * 266);
-  event.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  event.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
 })
