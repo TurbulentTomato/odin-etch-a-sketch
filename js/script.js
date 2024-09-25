9f143ee505b480f2636733358a2ffec40b909c5c
TurbulentTomato <abhinavgangwar87@gmail.com>
2024-09-25 17:48:53 +0530

    Add option to choose between single and multi color

--- js/script.js
+++ js/script.js
@@ -1,19 +1,39 @@
 const gridContainer = document.querySelector(".grid-container");
 const dimensionBtn = document.querySelector(".dimension-btn");
 let divNumber = 16; //no. of divs in grid
+const colorBtn = document.querySelector(".color-btn");
+
 renderGrid(divNumber);
+
+colorBtn.addEventListener("click", () => {
+  if (colorBtn.textContent === "COLORFUL MODE") {
+    colorBtn.textContent = "MONOCHROME MODE";
+  } else {
+    colorBtn.textContent = "COLORFUL MODE";
+  }
+})
+
+function getColor() {
+  let color = "black";
+  if (colorBtn.textContent === "COLORFUL MODE") {
+    const red = Math.floor(Math.random() * 266);
+    const green = Math.floor(Math.random() * 266);
+    const blue = Math.floor(Math.random() * 266);
+    const alpha = Math.round(Math.random() * 100) / 100;
+    color = `rgba(${red},
+                  ${green},
+                  ${blue},
+                  ${alpha})`;
+  }
+  return color;
+}
+
+
 gridContainer.addEventListener("mouseover", (event) => {
   if (event.target === gridContainer) {
     return;
   }
-  const red = Math.floor(Math.random() * 266);
-  const green = Math.floor(Math.random() * 266);
-  const blue = Math.floor(Math.random() * 266);
-  const alpha = Math.round(Math.random() * 100) / 100;
-  event.target.style.backgroundColor = `rgba(${red},
-                                             ${green},
-                                             ${blue},
-                                             ${alpha})`;
+  event.target.style.backgroundColor = getColor();
 })
 
 function renderGrid(divNumber) {
