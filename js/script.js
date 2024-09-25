131dde845b876f85f8d48198dd22e20ff622c156
TurbulentTomato <abhinavgangwar87@gmail.com>
2024-09-25 08:21:33 +0530

    Add function to change color on mouse hover.

--- js/script.js
+++ js/script.js
@@ -6,4 +6,9 @@ for (divNumber = divNumber ** 2; divNumber > 0; divNumber--) {
   container.appendChild(gridDiv);
 
 }
-
+container.addEventListener("mouseover", (event) => {
+  const red = Math.floor(Math.random() * 266);
+  const green = Math.floor(Math.random() * 266);
+  const blue = Math.floor(Math.random() * 266);
+  event.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+})
